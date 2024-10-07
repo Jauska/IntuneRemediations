c87b24bbce762de26629d174e77afb226e9b8b21
Jauska <niko@nevalainen.me>
2024-10-07 12:10:13 +0300

    Moving from General Scripts

---
+++ ConfigureWindowsFeatures/CheckFeatures.ps1
@@ -0,0 +1,42 @@
+try {
+    $Errors = $null
+    #Start-Transcript -Path "C:\Windows\Logs\Windows-FeaturesCheck.log"
+    $ToBeInstalled = "TelnetClient,TFTP,SimpleTCP,NetFx3"
+    $InstallFeatures = $ToBeInstalled.Split(",")
+    $ToBeRemoved = "WorkFolders-Client"
+    $RemoveFeatures = $ToBeRemoved.Split(",")
+    $CurrentState = Get-WindowsOptionalFeature -Online 
+    $Result = 0
+
+    ForEach ($FeatureName in $InstallFeatures) {
+        if (($CurrentState | Where-Object -Property "FeatureName" -eq -Value $FeatureName).State -eq "Enabled") {
+            Write-Host "Feature $($FeatureName) already installed."
+        } else {
+            Write-Host "Feature $($FeatureName) not installed."
+            $Result++
+
+        }
+    }
+    ForEach ($RFeatureName in $RemoveFeatures) {
+        if (('Disabled','DisabledWithPayloadRemoved').Contains(($CurrentState | Where-Object -Property "FeatureName" -eq -Value $FeatureName).State.ToString())) {
+            Write-Host "Feature $($RFeatureName) already uninstalled."
+        } elseif (($CurrentState | Where-Object -Property "FeatureName" -eq -Value $RFeatureName).State -eq "Enabled") {
+            Write-Host "Feature $($RFeatureName) not uninstalled."
+            $Result++
+        }
+    }
+
+}
+catch {
+    Write-Host "Installation failure! please see error below!"
+    $_
+    $Errors
+}
+finally {
+    #Stop-Transcript
+    if ($Result -eq 0) {
+        exit 0
+    }else{
+        exit 1
+    }
+}
---
+++ ConfigureWindowsFeatures/ConfigureFeatures.ps1
@@ -0,0 +1,42 @@
+﻿try {
+    $Errors = 
+    Start-Transcript -Path "C:\Windows\Logs\Windows-Features.log"
+    $ToBeInstalled = "TelnetClient,TFTP,SimpleTCP,NetFx3"
+    $InstallFeatures = $ToBeInstalled.Split(",")
+    $ToBeRemoved = "WorkFolders-Client"
+    $RemoveFeatures = $ToBeRemoved.Split(",")
+    $CurrentState = Get-WindowsOptionalFeature -Online 
+
+    #Installing features
+    ForEach ($FeatureName in $InstallFeatures) {
+        if (('Disabled','DisabledWithPayloadRemoved').Contains(($CurrentState | Where-Object -Property "FeatureName" -eq -Value $FeatureName).State.ToString())) {
+            Write-Host "Installing $($FeatureName)"
+            Enable-WindowsOptionalFeature -FeatureName $FeatureName -Online -NoRestart -ErrorAction Continue -ErrorVariable Errors
+        } elseif (($CurrentState | Where-Object -Property "FeatureName" -eq -Value $FeatureName).State -eq "Enabled") {
+            Write-Host "Feature $($FeatureName) already installed."
+        } else {
+            Write-Host "While installing $($FeatureName) got unknown installstate. WTF?"
+        }
+    }
+
+    #Removing features
+    ForEach ($RFeatureName in $RemoveFeatures) {
+        if (('Disabled','DisabledWithPayloadRemoved').Contains(($CurrentState | Where-Object -Property "FeatureName" -eq -Value $FeatureName).State).ToString()) {
+            Write-Host "Feature $($RFeatureName) already uninstalled."
+        } elseif (($CurrentState | Where-Object -Property "FeatureName" -eq -Value $RFeatureName).State -eq "Enabled") {
+            Write-Host "Removing $($RFeatureName)"
+            Disable-WindowsOptionalFeature -FeatureName $RFeatureName -Online -NoRestart -ErrorAction Continue -ErrorVariable Errors
+        } else {
+            Write-Host "While uninstalling $($RFeatureName) got unknown installstate. WTF?"
+        }
+    }
+
+}
+catch {
+    Write-Host "Installation failure! please see error below!"
+    $Errors
+    $_
+}
+finally {
+    Stop-Transcript
+}
